829d87ae44ec21877d9de8b87d23b850654f28f7
SeonJeong <sjkim5621@naver.com>
2024-12-17 20:16:30 +0900

    [SW-26] feat: base 코드 구현

---
+++ core/src/main/java/com/example/core/base/BaseViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.core.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<Event : UiEvent, State : UiState, Effect : UiSideEffect>(initialState: State) :
+    ViewModel() {
+
+    private val _uiState: MutableStateFlow<State> = MutableStateFlow(initialState)
+    val uiState = _uiState.asStateFlow()
+
+    private val _event: MutableSharedFlow<Event> = MutableSharedFlow()
+    val event = _event.asSharedFlow()
+
+    private val _effect: Channel<Effect> = Channel()
+    val effect = _effect.receiveAsFlow()
+
+    // Get Current State
+    val currentState: State
+        get() = _uiState.value
+
+    fun setEvent(event : Event) {
+        val newEvent = event
+        viewModelScope.launch { _event.emit(newEvent) }
+    }
+
+    protected fun setState(reduce: State.() -> State) {
+        val newState = currentState.reduce()
+        _uiState.value = newState
+    }
+
+    protected fun setEffect(builder: () -> Effect) {
+        val effectValue = builder()
+        viewModelScope.launch { _effect.send(effectValue) }
+    }
+
+}
---
+++ core/src/main/java/com/example/core/base/UiSideEffect.kt
@@ -0,0 +1,6 @@
+package com.example.core.base
+
+/**
+ * UiSideEffect: UI에서 발생하는 일회성 부수 효과를 나타냄
+ */
+interface UiSideEffect
