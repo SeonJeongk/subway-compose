a435d06e3933b40acc31350eedb8f32bd5729423
SeonJeong <sjkim5621@naver.com>
2024-12-17 18:24:42 +0900

    [SW-25] move: theme을 core 모듈로 옮기기

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -30,6 +30,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 }
 
 dependencies {
@@ -40,4 +46,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
+    implementation(libs.androidx.material3)
 }
