27d7421e5ccf754e5181e128e1689c71ab689c71
SeonJeongk <83932309+SeonJeongk@users.noreply.github.com>
2026-04-21 21:58:14 +0900

    [SW-34] feat: 홈화면 레이아웃 변경 및 문구 수정

--- feature/home/src/main/java/com/example/home/HomeScreen.kt
+++ feature/home/src/main/java/com/example/home/HomeScreen.kt
@@ -2,10 +2,13 @@ package com.example.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,13 +16,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.core.theme.SubwayTheme
 
 @Composable
 fun HomeScreen(
     onSubwayLineSelected: (String) -> Unit,
-    viewModel: HomeViewModel = hiltViewModel(),
 ) {
 
     Scaffold(
@@ -27,20 +28,32 @@ fun HomeScreen(
     ) { innerPadding ->
 
         Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(
+                space = 16.dp,
+                alignment = Alignment.CenterVertically,
+            ),
         ) {
             Text(
-                "Home",
-                modifier = Modifier.padding(innerPadding),
+                text = "서울 지하철 호선별 역 정보",
+                style = MaterialTheme.typography.headlineMedium,
             )
-
-            // 선택된 노선 전달
-            Button(onClick = { onSubwayLineSelected("1호선") }) {
+            Text(text = "확인할 호선을 선택해 주세요.")
+            Spacer(Modifier.height(30.dp))
+            Button(
+                onClick = { onSubwayLineSelected("1호선") },
+                modifier = Modifier.fillMaxWidth(),
+            ) {
                 Text("1호선 역 정보로 이동")
             }
-            Button(onClick = { onSubwayLineSelected("2호선") }) {
+            Button(
+                onClick = { onSubwayLineSelected("2호선") },
+                modifier = Modifier.fillMaxWidth(),
+            ) {
                 Text("2호선 역 정보로 이동")
             }
         }
